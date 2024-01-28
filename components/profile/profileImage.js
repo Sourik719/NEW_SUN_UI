b3b5560979cc63ec878fb20b945e1e9e79cd0df6
Sourik Bhuiya <sourikbhuiya@gmail.com>
2024-01-28 15:07:03 +0530

    Profile Image Uploading Updated

--- components/profile/profileImage.js
+++ components/profile/profileImage.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import blank from "../../public/blank.jpeg";
 import female_blank from "../../public/female_blank.jpg";
 import male_blank from "../../public/male_blank.jpg";
+
 
 const ProfileImage = ({ label, value, gender }) => {
     const [uploadState, setUploadState] = useState(false);
@@ -10,46 +11,44 @@ const ProfileImage = ({ label, value, gender }) => {
 
     const handleImageChange = (e) => {
         const file = e.target.files[0];
-        setUploadState(!uploadState);
+        setUploadState(false);
         const url = URL.createObjectURL(file);
         setImageURL(url);
     };
-    const handleUpload = () => {
-        setUploadState(true);
-    };
-
 
     return (
         <div className="mb-2 mt-2">
-            <div className="relative mt-1">
-                <div className="relative w-40 h-40 rounded-full overflow-hidden border-2 border-black" onClick={handleUpload}>
-                    {imageURL === null ? (
-                        <Image
-                            src={gender === "Male" ? male_blank : (gender === "Female" ? female_blank : blank)}
-                            alt="Selected"
-                            className="w-full h-full object-cover"
-                        />
-                    ) : (
-                        <Image
-                            src={imageURL}
-                            alt="Selected"
-                            layout="fill"
-                            className="w-full h-full object-cover"
-                        />
-                    )}
-
-                    {uploadState && (<input
+            <div className="relative group w-40 h-40 rounded-full overflow-hidden border-2 border-black cursor-pointer"
+                onMouseEnter={() => setUploadState(true)}
+                onMouseLeave={() => setUploadState(false)}>
+                {uploadState && <label className="absolute w-full h-full bg-black bg-opacity-60 flex items-center justify-center text-gray-200 z-50">
+                    Edit
+                    <input
                         type="file"
                         accept="image/*"
                         onChange={handleImageChange}
-                        onClick={() => console.log("Clicked")}
-                        className="absolute inset-0 opacity-0 cursor-pointer w-full h-full"
+                        className="absolute inset-0 opacity-0 cursor-pointer w-full h-full z-100"
                         id="image-upload"
                     />
-                    )}
-                </div>
+                </label>}
+                {imageURL === null ? (
+                    <Image
+                        src={gender === "Male" ? male_blank : (gender === "Female" ? female_blank : blank)}
+                        alt="Selected"
+                        className="w-full h-full object-cover"
+                    />
+                ) : (
+                    <Image
+                        src={imageURL}
+                        alt="Selected"
+                        layout="fill"
+                        className="w-full h-full object-cover"
+                    />
+                )}
+
 
             </div>
+
         </div>
 
     );
