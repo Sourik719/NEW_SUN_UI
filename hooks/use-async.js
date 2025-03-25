2b869d5665e9f0588a5a29d76f38904a1085f485
Sourik Bhuiya <sourikbhuiya@gmail.com>
2025-03-25 19:40:14 +0530

    parameter passed

--- hooks/use-async.js
+++ hooks/use-async.js
@@ -1,11 +1,11 @@
-import { useDispatch } from "react-redux"
 import { notificationActions } from "@/store/notification-slice"
+import { useDispatch } from "react-redux"
 
 export const useAsync = () => {
     const dispatch = useDispatch()
     const catchAsync = asyncFunction => {
-        return () => {
-            asyncFunction().catch(error => {
+        return (response) => {
+            asyncFunction(response).catch(error => {
                 dispatch(notificationActions.setNotification({
                     type: 'error',
                     message: error.message
