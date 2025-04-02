65d31a443e6f17c914406d2b0b0c7c7a55f98305
Sourik Bhuiya <sourikbhuiya@gmail.com>
2025-04-02 12:38:24 +0530

    imports updated

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Carousel from "@/components/home/Carousel"
-import Contact from "@/components/home/Contact"
-import FeedbackCont from "@/components/home/feedbackView"
-import Participation from "@/components/home/Participation"
-import Projects from "@/components/home/Projectsection"
-import Purpose from "@/components/home/Purpose"
-import Container from "@/components/ui/Container"
+import Carousel from "@/components/home/Carousel.js"
+import Contact from "@/components/home/Contact.js"
+import FeedbackCont from "@/components/home/feedbackView.js"
+import Participation from "@/components/home/Participation.js"
+import Projects from "@/components/home/Projectsection.js"
+import Purpose from "@/components/home/Purpose.js"
+import Container from "@/components/ui/Container.js"
 import Head from "next/head"
 
 const Home = () => {
