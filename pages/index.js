4aee02369215e460880e65caa44d80ecf0d1327e
Sourik Bhuiya <sourikbhuiya@gmail.com>
2025-04-03 16:52:19 +0530

    Home page index updated

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Head from "next/head"
 const Home = () => {
     return (<Container>
         <Head>
-            <title>Home</title>
+            <title>Home || TEAM NEW SUN FOUNDATION</title>
         </Head>
         <Carousel />
         <Purpose />
