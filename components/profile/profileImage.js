1205d16b6740fd66598a2e810ba1cab67d2ef738
Sourik Bhuiya <sourikbhuiya@gmail.com>
2025-04-05 11:23:34 +0530

    Delete image feature added

--- components/profile/profileImage.js
+++ components/profile/profileImage.js
@@ -4,7 +4,7 @@ import { notificationActions } from "@/store/notification-slice";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef, useState } from "react";
-import { FaCamera } from "react-icons/fa";
+import { FaCamera, FaTrashCan, FaUpload, FaXmark } from "react-icons/fa6";
 import { useDispatch } from "react-redux";
 import blank from "../../public/blank.png";
 import female_blank from "../../public/female_blank.jpg";
@@ -45,7 +45,6 @@ const ProfileImage = ({ label, value, gender, id, editAble }) => {
         if (selectedFile) {
             const updateData = new FormData();
             updateData.append("image", selectedFile);
-            setUploadState(false);
             const responseData = await httpRequest(`/members/${id}/image`, 'PUT', updateData, true);
             if (responseData) {
                 setPreviewURL(responseData.data?.member?.image?.url);
@@ -54,12 +53,21 @@ const ProfileImage = ({ label, value, gender, id, editAble }) => {
                 }));
                 router.reload();
             }
-        } else {
-            dispatch(notificationActions.setNotification({
-                message: 'No new image selected.'
-            }));
         }
     });
+    const handleDeleteImage = catchAsync(async () => {
+        const updateData = new FormData();
+        updateData.append("image", null);
+        const responseData = await httpRequest(`/members/${id}/image`, 'PUT', updateData, true);
+        if (responseData) {
+            setPreviewURL(responseData.data?.member?.image?.url);
+            dispatch(notificationActions.setNotification({
+                message: responseData.message
+            }));
+            router.reload();
+        }
+    });
+
 
     const handleCancelEdit = () => {
         setSelectedFile(null);
@@ -79,40 +87,48 @@ const ProfileImage = ({ label, value, gender, id, editAble }) => {
     return (
         <div className="mb-4">
             {label && <label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>}
-            <div className="relative group w-40 h-40 rounded-full overflow-hidden border-2 border-gray-300 hover:border-indigo-500 transition-all duration-200 shadow-md" onMouseEnter={editAble ? handleMouseEnter :null} onMouseLeave={handleMouseLeave}>
+            <div className="relative group w-40 h-40 rounded-full overflow-hidden border-2 border-gray-300 hover:border-indigo-500 transition-all duration-200 shadow-md" onMouseEnter={editAble ? handleMouseEnter : null} onMouseLeave={handleMouseLeave}>
                 {(uploadState || isEditing) && (
-                    <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center text-gray-100 z-50 cursor-pointer transition-opacity duration-200">
-                        {!isEditing ? (
-                            <div className="flex items-center justify-center text-lg">
-                                <FaCamera className="mr-2" />
-                                <span className="hidden sm:inline">Change</span>
+                    <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center text-gray-100 z-50  transition-opacity duration-200">
+                        {!isEditing ? <div className="flex flex-col items-center justify-center">
+                            <div className="flex flex-col ">
+                                <button
+                                    onClick={() => fileInputRef.current?.click()}
+                                    className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-1 px-2 rounded text-sm focus:outline-none focus:shadow-outline my-2"
+                                    disabled={isLoading}
+                                >
+                                    <div className="flex flex-row items-center justify-center"><FaCamera /> <span className="text-sm inline m-0.5"> Change</span></div>
+                                </button>
                                 <input
                                     type="file"
                                     accept="image/*"
                                     onChange={handleFileChange}
-                                    className="absolute inset-0 opacity-0 cursor-pointer w-full h-full z-100"
+                                    className="hidden"
                                     id="image-upload"
                                     ref={fileInputRef}
                                 />
+                                {previewURL && <button
+                                    onClick={handleDeleteImage}
+                                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded text-sm focus:outline-none focus:shadow-outline my-2"
+                                    disabled={isLoading}
+                                >
+                                    <div className="flex flex-row items-center justify-center"><FaTrashCan /> <span className="text-sm inline m-0.5"> Delete</span></div>
+                                </button>}
                             </div>
-                        ) : (
+                        </div> : (
                             <div className="flex flex-col items-center justify-center">
-                                <div className="flex items-center mb-2">
-                                    <FaCamera className="mr-2" />
-                                    <span>New Image</span>
-                                </div>
-                                <div className="flex space-x-2">
+                                <div className="flex flex-col">
                                     <button
                                         onClick={handleUpdate}
-                                        className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-1 px-2 rounded text-sm focus:outline-none focus:shadow-outline"
+                                        className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-1 px-2 rounded text-sm focus:outline-none focus:shadow-outline my-2"
                                     >
-                                        Update
+                                        <div className="flex flex-row items-center justify-center"><FaUpload /> <span className="text-sm inline m-0.5"> Upload</span></div>
                                     </button>
                                     <button
                                         onClick={handleCancelEdit}
-                                        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded text-sm focus:outline-none focus:shadow-outline"
+                                        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded text-sm focus:outline-none focus:shadow-outline my-2"
                                     >
-                                        Cancel
+                                        <div className="flex flex-row items-center justify-center"><FaXmark /> <span className="text-sm inline m-0.5"> Cancel </span></div>
                                     </button>
                                 </div>
                             </div>
@@ -144,7 +160,7 @@ const ProfileImage = ({ label, value, gender, id, editAble }) => {
                     </div>
                 )}
             </div>
-        </div>
+        </div >
     );
 };
 
