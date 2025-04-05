029564c422bd23ae47a06cbe6de9a9b011ec250e
Sourik Bhuiya <sourikbhuiya@gmail.com>
2025-04-05 21:38:01 +0530

    Team members added

--- components/about/Team.js
+++ components/about/Team.js
@@ -68,9 +68,19 @@ const Team = () => {
                     imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/c_crop,g_north,h_800,w_800/core-member/rslvcnphfbfe6rxjo40p"
                 />
                 <PositionHolder
+                    name="Sudipa Saha"
+                    position="Head of Cultural Cell"
+                    imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/v1743867889/core-member/p2tp1k3loefuywwxcqyv.jpg"
+                />
+                <PositionHolder
                     name="Niladri Banerjee"
                     position="Project Coordinator-Boighor"
                     imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/v1743071037/core-member/fhq3l8wl3nwssdo3spp4.jpg"
+                />
+                <PositionHolder
+                    name="Sankalpa Basak"
+                    position="Project Coordinator-Boighor"
+                    imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/v1743867885/core-member/twmwmncs2f1ckqv7rjsp.jpg"
                 />
                 <PositionHolder
                     name="Sohon Biswas"
@@ -78,9 +88,19 @@ const Team = () => {
                     imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/v1743616542/core-member/czrhfjdkgphjtozr72em.jpg"
                 />
                 <PositionHolder
+                    name="Moitrayi Majumdar"
+                    position="Project Coordinator-SUNSHINE"
+                    imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/v1743867888/core-member/uev4lkn9ldk12nobwsmf.jpg"
+                />
+                <PositionHolder
                     name="Debanjan Mandal"
                     position="Project Coordinator-Sobujer Sondhane"
                     imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/v1743071039/core-member/vtsa5qkpijwtjoeifkfj.jpg"
+                />
+                <PositionHolder
+                    name="Shibam Sanyal"
+                    position="Project Coordinator-Sobujer Sondhane"
+                    imageUrl="https://res.cloudinary.com/dcikuo4sk/image/upload/v1743867884/core-member/ivnuwe0y13fusmoj2hza.jpg"
                 />
             </div>
             <p className="text-yellow-500 font-bold text-center">Our organization's strength and impact are a testament to the collective efforts of many. Beyond our core team, dedicated volunteers tirelessly contribute their time and skills during our projects. We are also deeply grateful to our esteemed well-wishers, whose invaluable guidance and feedback shape our direction. Finally, the unwavering belief and generous donations from our supporters enable us to continue our vital work and bring our projects to fruition.
