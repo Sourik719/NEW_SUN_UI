da3ab3dafa5b75da6042069fbd931419a55d4dbe
Sourik Bhuiya <sourikbhuiya@gmail.com>
2025-04-05 11:24:58 +0530

    Minor spelling correction

--- components/home/Participation.js
+++ components/home/Participation.js
@@ -15,7 +15,7 @@ const Participation = () => {
         <div className="flex flex-col items-center justify-center bg-black text-white py-5 p-2">
             <p className="text-3xl text-center">Want to be a part of <span className="mr-1 text-orange-400">our journey?</span></p>
             <div className="w-full flex sm:flex-row flex-col p-5 justify-center items-center">
-                <EngageBtn label="Sponsor a Event" href={"/donate/sponsor"} />
+                <EngageBtn label="Sponsor an Event" href={"/donate/sponsor"} />
                 {!isAuthenticated && <EngageBtn label="Become a Member" href={"/join-us"} />}
                 {isAuthenticated && <EngageBtn label="Monthly Contribution" href={"/donate/member_contribution"} />}
                 <EngageBtn label="Donate for a Cause" href={"/donate/cause"} />
