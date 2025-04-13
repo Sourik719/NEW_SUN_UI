e0e4ed68cf0b04ac0e036b9d4da89d8e11e3034c
Sourik Bhuiya <sourikbhuiya@gmail.com>
2025-04-13 17:19:06 +0530

    Privacy,cancel and terms added

---
+++ pages/cancellation.js
@@ -0,0 +1,11 @@
+import Container from '@/components/ui/Container';
+
+const Cancellations = () => {
+    return (
+        <Container className="container mx-auto p-4 max-w-3xl">
+            <h1>No cancellations and refunds available</h1>
+        </Container>
+    );
+};
+
+export default Cancellations;
